a3ce311e19a4abf4fa919199a3722f20422613ea
d-theus <slma0x02@gmail.com>
2015-02-11 11:45:27 +0300

    describe executable enhancements

--- lib/rspec/launchbox.rb
+++ lib/rspec/launchbox.rb
@@ -5,3 +5,9 @@ require 'rspec/its'
 require 'rspec/launchbox/timeout_matchers.rb'
 require 'rspec/launchbox/in_presence_of.rb'
 require 'rspec/launchbox/describe_executable.rb'
+
+include RSpec::DescribeExecutable
+RSpec.configure do |c|
+  c.extend RSpec::InPresenceOf
+  c.include RSpec::Matchers::Timeout
+end
