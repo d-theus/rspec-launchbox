e1c5debb1975a1e7319e80d8399cf7431300f330
d-theus <slma0x02@gmail.com>
2015-02-11 11:45:09 +0300

    timeouts spec fix

--- spec/timeouts_spec.rb
+++ spec/timeouts_spec.rb
@@ -1,15 +1,23 @@
 describe '#persist' do
   context 'with jush value passed' do
     it 'raises exceptions' do
-      expect(5).to persist 5
+      begin
+        expect(5).to persist 5
+      rescue RuntimeError => e
+        raise e unless e.message.include? 'Block expected'
+      end
     end
   end
   context 'with block passed' do
     context 'with no boundary' do
       it 'd' do
-        expect do
-          'string'
-        end.to persist
+        begin
+          expect do
+            'string'
+          end.to persist
+        rescue RuntimeError => e
+          raise e unless e.message.include? 'Expected'
+        end
       end
     end
 
@@ -29,10 +37,14 @@ describe '#persist' do
 
   context 'with block which raises exception' do
     it 'rethrows' do
-      expect do
-        'string'
-        fail 'Exception in subject block'
-      end.to persist
+      begin
+        expect do
+          'string'
+          fail 'Exception in subject block'
+        end.to persist.at_most(1)
+      rescue RuntimeError => e
+        raise e unless e.message.include? 'Exception in subject block'
+      end
     end
   end
 end
